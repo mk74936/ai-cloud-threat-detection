4b602181e21d861b25fe12127bbdd186c62edeb7
mk74936 <117074826+mk74936@users.noreply.github.com>
2025-06-03 13:50:47 -0400

    Trigger workflow

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "azure_subscription_id" {}
+variable "azure_tenant_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
